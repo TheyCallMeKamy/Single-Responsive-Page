ea1bfda49be5f85cbcd7781dc3294bf58cc9a09b
TheyCallMeKamy <112433005+TheyCallMeKamy@users.noreply.github.com>
2024-09-13 10:54:45 +0100

    hamburger

---
+++ js/hamNav.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("pageSideNav").style.width = "200px";
+
+}
+
+
+function closeNav() {
+    document.getElementById("pageSideNav").style.width = "0";
+}
